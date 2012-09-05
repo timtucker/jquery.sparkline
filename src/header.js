c1890d0241e79192308bccb937ffc9f72b17095a
Tim Tucker <tim@timtucker.com>
2012-09-05 11:38:57 -0300

    Update src/header.js
    Closure to cache document, Math, undefined -- should result in a very slight performance improvement and smaller minified size.

--- src/header.js
+++ src/header.js
@@ -202,6 +202,8 @@
 
 /*jslint regexp: true, browser: true, jquery: true, white: true, nomen: false, plusplus: false, maxerr: 500, indent: 4 */
 
+(function( document, Math, undefined ) {
+
 (function(factory) {
     if(typeof define === 'function' && define.amd) {
 		define(['jquery'], factory);
